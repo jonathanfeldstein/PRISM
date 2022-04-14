e2d60e1dd137b4425d58dedf07cace14a31d7ebc
Jonathan Feldstein <jonathan.feldstein@ed.ac.uk>
2022-04-14 12:14:57 +0100

    Implemented commonly encountered nodes

--- Utils/clustering_utils.h
+++ Utils/clustering_utils.h
@@ -12,7 +12,7 @@ using namespace std;
 using namespace boost;
 
 double compute_theta_sym(double alpha_sym, size_t number_of_walks_ran, size_t length_of_walk);
-set<NodeRandomWalkData> get_commonly_encountered_nodes(map<size_t, NodeRandomWalkData>);
+set<NodeRandomWalkData> get_commonly_encountered_nodes(map<size_t, NodeRandomWalkData> nodes_random_walk_data, size_t number_of_walks_ran, double epsilon);
 pair<set<size_t>, vector<set<size_t>>> cluster_nodes_by_path_similarity(vector<NodeRandomWalkData> nodes_of_type,
 size_t number_of_walks_ran,
 double theta_sym,
