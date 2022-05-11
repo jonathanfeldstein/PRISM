0f6d26e205c379f7f1295f5cf7fd449cdb201902
dominicp6 <dom.phillips97@gmail.com>
2022-05-11 17:05:11 +0100

    split unconnected hypergraph before running get communities

--- main.cpp
+++ main.cpp
@@ -10,72 +10,66 @@ using namespace std;
 
 int main(int argc, char** argv)
 {
-    HyperGraph hg = HyperGraph("/home/dominic/CLionProjects/FASTER/Databases/unconnected_hypergraph_test.db",
-                               "/home/dominic/CLionProjects/FASTER/Databases/unconnected_hypergraph_test.info",
-                               false);
+    if(argc >= 6){
+        string db_file_path = argv[1];
+        string info_file_path = argv[2];
+        string output_filename = argv[3];
+        bool multiprocessing = true;
+        if(argc == 7){
+            multiprocessing = stoi(argv[6]);
+            if(multiprocessing == 0){
+                cout << "NO MULTIPROCESSING" << endl;
+            }else{
+                cout << "MULTIPROCESSING" << endl;
+            }
+        }else{
+            cout << "MULTIPROCESSING" << endl;
+        }
 
-    vector<set<NodeId>> hgs = hg.find_unconnected_components();
-    vector<HyperGraph> constructed_hypergraphs;
-    for(auto component:hgs){
-        HyperGraph subgraph(component, hg);
-        constructed_hypergraphs.emplace_back(subgraph);
+        Timer timer("main");
+
+        Timer timer_readfile("Read Files");
+        HyperGraph original_hg(argv[1], argv[2], false);
+        vector<HyperGraph> hgs = construct_hypergraphs_from_files(argv[1], argv[2], false);
+        timer_readfile.Stop();
+
+        Timer timer_hc("HC");
+        HierarchicalClustererConfig config;
+        config.max_lambda2 = stod(argv[7]);
+        config.min_cluster_size = stoi(argv[8]);
+        vector<HyperGraph> full_hc_clusters;
+        for (auto hg: hgs) {
+            HierarchicalClusterer hc(hg, config);
+            vector<HyperGraph> hc_clusters = hc.run_hierarchical_clustering();
+            full_hc_clusters.insert(full_hc_clusters.end(), hc_clusters.begin(), hc_clusters.end());
+        }
+        timer_hc.Stop();
+
+        Timer timer_rw("RW");
+        RandomWalkerConfig config_rw{};
+        config_rw.epsilon = stod(argv[4]);
+        config_rw.alpha = stod(argv[5]);
+        config_rw.multiprocessing = multiprocessing;
+
+        vector<Communities> com_vector;
+        for (auto HyperG: full_hc_clusters) {
+            cout << "---------------------------------------------------------------------------------------\n";
+            HyperG.print();
+            Communities com = Communities(HyperG, config_rw);
+            com.print();
+            com_vector.emplace_back(com);
+        }
+        timer_rw.Stop();
+
+        Timer timer_communities("Communities Printer");
+        CommunityPrinter com_printer = CommunityPrinter(com_vector, original_hg);
+        com_printer.write_files(output_filename);
+        timer_communities.Stop();
+
+        timer.Stop(); // Timing of full program
+    }else{
+        string path = argv[1];
+        bool output = RunAllTests(path);
     }
-    cout << "!";
-//    if(argc >= 6){
-//        string db_file_path = argv[1];
-//        string info_file_path = argv[2];
-//        string output_filename = argv[3];
-//        bool multiprocessing = true;
-//        if(argc == 7){
-//            multiprocessing = stoi(argv[6]);
-//            if(multiprocessing == 0){
-//                cout << "NO MULTIPROCESSING" << endl;
-//            }else{
-//                cout << "MULTIPROCESSING" << endl;
-//            }
-//        }else{
-//            cout << "MULTIPROCESSING" << endl;
-//        }
-//
-//        Timer timer("main");
-//
-//        Timer timer_readfile("Read Files");
-//        HyperGraph hg(argv[1], argv[2], false);
-//        timer_readfile.Stop();
-//
-//        Timer timer_hc("HC");
-//        HierarchicalClustererConfig config;
-//        config.max_lambda2 = stod(argv[7]);
-//        config.min_cluster_size = stoi(argv[8]);
-//        HierarchicalClusterer hc(hg, config);
-//        vector<HyperGraph> hc_clusters = hc.run_hierarchical_clustering();
-//        timer_hc.Stop();
-//
-//        Timer timer_rw("RW");
-//        RandomWalkerConfig config_rw{};
-//        config_rw.epsilon = stod(argv[4]);
-//        config_rw.alpha = stod(argv[5]);
-//        config_rw.multiprocessing = multiprocessing;
-//
-//        vector<Communities> com_vector;
-//        for (auto HyperG: hc_clusters) {
-//            cout << "---------------------------------------------------------------------------------------\n";
-//            HyperG.print();
-//            Communities com = Communities(HyperG, config_rw);
-//            com.print();
-//            com_vector.emplace_back(com);
-//        }
-//        timer_rw.Stop();
-//
-//        Timer timer_communities("Communities Printer");
-//        CommunityPrinter com_printer = CommunityPrinter(com_vector, hg);
-//        com_printer.write_files(output_filename);
-//        timer_communities.Stop();
-//
-//        timer.Stop(); // Timing of full program
-//    }else{
-//        string path = argv[1];
-//        bool output = RunAllTests(path);
-//    }
     return 0;
 }
