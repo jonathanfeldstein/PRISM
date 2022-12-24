f62e6b15d650ed1b40de82736e5075c0b28ce756
Jonathan Feldstein <jonathan.feldstein@ed.ac.uk>
2022-12-24 22:20:51 +0000

    Find cliques implemented

--- Motifs/Motif.h
+++ Motifs/Motif.h
@@ -10,6 +10,7 @@
 #include <set>
 #include <vector>
 #include <string>
+#include <omp.h>
 #include "Concepts.h"
 using namespace std;
 
@@ -23,8 +24,26 @@ class Motif : public HyperGraph {
 
 private:
     NodeId source_concept;
+
+    // Function to check whether a given set of nodes form a clique
+    bool is_clique(std::set<NodeId> nodes);
+
+    // Function to check whether two nodes are neighbors in the hypergraph
+    bool are_neighbors(NodeId node1, NodeId node2);
+
+    // Recursive function to find all cliques of a given size in the hypergraph
+    std::vector<std::set<NodeId>> find_cliques_of_size(std::set<NodeId> current_clique, size_t clique_size);
+
+
 public:
     Motif(Concept &abstract_concept, HyperGraph hypergraph);
+    // Function to get the neighbors of a given node
+    std::set<NodeId> get_neighbors(NodeId node);
+
+    // Function to find all cliques in the hypergraph up to a maximum size
+    // Recursive function to find all cliques in the hypergraph up to a maximum size
+    // Function to find all cliques in the hypergraph up to a maximum size
+    std::vector<std::set<NodeId>> find_cliques(size_t max_size);
 };
 
 
